a552cf311435289c2c779b53e09dadf64c85e9e4
MohammedAliZaini <mazaini93@gmail.com>
2024-12-11 09:33:48 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./components/Hello";
+import "./App.css";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,18 @@
+import "../pages/home.css";
+
+const Home = () => {
+  return (
+    <>
+    <span className="key">W</span>
+    <span className="key">e</span>
+    <span className="key">l</span>
+    <span className="key">c</span>
+    <span className="key">o</span>
+    <span className="key">m</span>
+    <span className="key">e</span>
+    <span className="key">!</span>
+    </>
+  )
+}
+
+export default Home;
